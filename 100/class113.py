0c97a09d3f17ea88e7630a64057efe8c573cd4b8
polamreddyGnaneswarreddy <77733853+polamreddyGnaneswarreddy@users.noreply.github.com>
2022-06-21 18:48:21 +0530

    Add files via upload

---
+++ 100/class113.py
@@ -0,0 +1,21 @@
+class Car:
+    def __init__(self,model,color,company,speed_limit):
+    self.color=color
+    self.model=model
+    self.company=company
+    self.speed_limit=speed_limit
+
+def start(self):
+    print("started")
+
+def stop(self):
+    print("stopped")
+
+def accelarate(self):
+    print("accelarating...")
+    "accelarate functionality here"
+
+def change_gear(self,gear_type):
+    print("gear changed")
+    "gear changed functionality here"
+
